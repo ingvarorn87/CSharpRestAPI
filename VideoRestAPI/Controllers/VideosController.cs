2bbfff4dce352b95256e7c2933dbad5ef45f4937
ingvarorn87 <ingvarorn87@gmail.com>
2017-09-20 10:19:44 +0200

    Many changes

--- VideoRestAPI/Controllers/VideosController.cs
+++ VideoRestAPI/Controllers/VideosController.cs
@@ -2,42 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using VideoManagerBLL.BusinessObjects;
 
 namespace VideoRestAPI.Controllers
 {
+    [Produces("application/json")]
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class VideosController : Controller
     {
-        // GET api/values
+        // GET: api/Videos
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "Hello" };
+            return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
+        // GET: api/Videos/5
+        [HttpGet("{id}", Name = "Get")]
         public string Get(int id)
         {
-
-            return "value  --  " + id;
+            return "value";
         }
-
-        // POST api/values
+        
+        // POST: api/Videos
         [HttpPost]
-        public void Post([FromBody]VideoBO vid)
+        public void Post([FromBody]string value)
         {
         }
-
-        // PUT api/values/5
+        
+        // PUT: api/Videos/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
-
-        // DELETE api/values/5
+        
+        // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
