1d60597264fa1ed2cb5569cab676d42b142ce9ed
ingvarorn87 <ingvarorn87@gmail.com>
2017-11-06 18:53:21 +0100

    Customer Entity added
    New entity but no backend

---
+++ VideoManagerDAL/Entities/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoManagerDAL.Entities
+{
+    class Customer
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public int Id { get; set; }
+        public String AddressName { get; set; }
+
+    }
+}
